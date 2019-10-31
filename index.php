942d34c0899b8adb1ff0645dd8ce1c863c31ae0b
Austerzockt <48381097+austerzockt@users.noreply.github.com>
2019-10-31 17:47:15 +0100

    Commit #3
    Signed-off-by: Austerzockt <48381097+Austerzockt@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+<p>by @Austerzockt 2019</p>
+<a href ="page2.html">Page 2</a>
+<br>
+<?php
+    $test = 1;
+    echo $test;
+?>
+
+
+</body>
+</html>
